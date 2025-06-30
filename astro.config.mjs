c0f3ccb5a2622dbe0eb27856fca104293ba9aa74
Ryohei Tanaka <ryhe@users.noreply.github.com>
2025-06-30 20:43:55 +0900

    feat: bootstrap Astro blog

---
+++ astro.config.mjs
@@ -0,0 +1,19 @@
+// https://astro.build/config
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import icon from "astro-icon";
+
+import { remarkReadingTime } from "./src/utils/all";
+
+export default defineConfig({
+  site: "https://ryhe.github.io",
+  base: '/malaysia-life-blog/',
+  markdown: {
+    remarkPlugins: [remarkReadingTime],
+    rehypePlugins: ["rehype-plugin-image-native-lazy-loading"],
+    extendDefaultPlugins: true,
+  },
+  integrations: [tailwind(), mdx(), sitemap(), icon()],
+});
